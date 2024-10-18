ee1cb08b9c310412a6b7317abb60b824ab6b304c
kosta <user@WIN-7DMJIC53KE9>
2024-10-18 21:31:29 +0200

    establish connection to db and send some test message

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from typing import Union
- 
+from pymongo import MongoClient
+import os
 from fastapi import FastAPI
  
 app = FastAPI()
+
+def get_database():
  
+   # Provide the mongodb atlas url to connect python to mongodb using pymongo
+   # os.environ (variable name, default value if variable is null)
+   CONNECTION_STRING = os.environ.get("DATABASE_URL", "mongodb://localhost:27017/")
+ 
+   # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+   client = MongoClient(CONNECTION_STRING)
+ 
+   # Create or connect the database for our example (we will use the same database throughout the tutorial
+   return client["school"]
+
+def get_students():
+
+    dbname = get_database()
+    # Retrieve a collection named "students" from database
+    #collection_name = dbname["students"]
+    res = "All collections: "
+    for coll in dbname.list_collection_names():
+         res += coll
+    return res
+   
+
 @app.get("/")
 def read_root():
-    return {"message": "Guten Tag Frau Pyzh, Ich möchte dich küssen :) "}
+    res = get_students()
+    return {"message": "Guten Tag Frau Pyzh, Ich möchte dich küssen :) " + res}
