4ec9fc31879e14536a6caad3cb1d8a43aa9d7a9a
kosta <user@WIN-7DMJIC53KE9>
2024-10-18 22:03:30 +0200

    add certificate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
 from pymongo import MongoClient
 import os
+import certifi
 from fastapi import FastAPI
  
 app = FastAPI()
@@ -10,10 +11,11 @@ def get_database():
    # Provide the mongodb atlas url to connect python to mongodb using pymongo
    # os.environ (variable name, default value if variable is null)
    CONNECTION_STRING = os.environ.get("DATABASE_URL", "mongodb://localhost:27017/")
- 
+
+   ca = certifi.where()
    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
-   client = MongoClient(CONNECTION_STRING)
- 
+   client = MongoClient(CONNECTION_STRING, tlsCAFile=ca)
+
    # Create or connect the database for our example (we will use the same database throughout the tutorial
    return client["school"]
 
